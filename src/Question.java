ccf6c6c63a327182cd0b46a05783243aeee98280
ameenkhabbaz <ameen.khabbaz@gmail.com>
2020-11-13 21:33:54 -0500

    User insert question

---
+++ src/Question.java
@@ -0,0 +1,62 @@
+public class Question {
+	protected int questionID;
+	protected String question;
+	protected String email;
+	protected String tag;
+	protected String date;
+
+	public Question() {
+	}
+
+	public Question(int questionID, String question, String email, String tag) {
+		this.questionID = questionID;
+		this.question = question;
+		this.email = email;
+		this.tag = tag;
+	}
+		
+	public Question(int questionID, String question, String email, String tag, String date) {
+		this(questionID, question, email, tag);
+		this.date = date;
+	}
+	
+	public int getQuestionID() {
+		return questionID;
+	}
+	
+	public void setQuestionID(int questionID)  {
+		this.questionID = questionID;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+	
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	
+	public String getTag() {
+		return tag;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+}
---
+++ src/QuestionDAO.java
@@ -0,0 +1,122 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/QuestionDAO")
+public class QuestionDAO {
+	private static final long serialVersionUID = 1L;
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	public QuestionDAO() {
+		
+	}
+	
+     /**
+     * @see HttpServlet#HttpServlet()
+     */
+    protected void connect_func() throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager
+  			      .getConnection("jdbc:mysql://127.0.0.1:3306/testdb?&serverTimezone=UTC&"
+  			          + "user=john&password=pass1234");
+            statement = connect.createStatement();
+        }
+    }
+    
+    protected void disconnect() throws SQLException {
+        if (connect != null && !connect.isClosed()) {
+        	connect.close();
+        }
+    }
+
+	public void addQuestion(String email, String question, String tag) throws SQLException {
+    	connect_func();         
+	String sql = "insert into Question(email, question, tag) values (?, ?, ?)";
+	preparedStatement = (PreparedStatement) connect.prepareStatement(sql);
+	preparedStatement.setString(1, email);
+	preparedStatement.setString(2, question);
+	preparedStatement.setString(3, tag);
+		
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        statement.close();
+        disconnect();    	
+    }
+    
+    public void removeQuestion(String email, String question, String tag) throws SQLException {
+    	connect_func();         
+	String sql = "DELETE FROM Questions WHERE email = ? AND question = ? AND tag = ?";
+	preparedStatement = (PreparedStatement) connect.prepareStatement(sql);
+	preparedStatement.setString(1, email);
+	preparedStatement.setString(2, question);
+	preparedStatement.setString(3, tag);
+		
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        statement.close();
+        disconnect();    	
+    }
+    
+    public Question getQuestion(String email, String question, String tag) throws SQLException {
+    	connect_func();         
+		String sql = "Select * FROM Question where email = ? AND url = ? AND tag =?";
+		preparedStatement = (PreparedStatement) connect.prepareStatement(sql);
+		preparedStatement.setString(1, email);
+		preparedStatement.setString(2, question);
+		preparedStatement.setString(3, tag);
+
+        resultSet = preparedStatement.executeQuery();
+        Question temp = new Question();
+        while (resultSet.next()) {
+        	temp.setQuestionID(resultSet.getInt("questionID"));
+        	temp.setEmail(resultSet.getString("email"));
+            temp.setQuestion(resultSet.getString("question"));
+            temp.setTag(resultSet.getString("tag"));
+            temp.setDate(resultSet.getString("date"));
+        }
+        preparedStatement.close();
+        statement.close();
+        resultSet.close();
+        disconnect();
+        return temp;
+    	
+    }
+
+    public List<String> usersQuestion(String email) throws SQLException {
+    	connect_func();         
+		String sql = "Select questionid FROM Question where email = ?";
+		preparedStatement = (PreparedStatement) connect.prepareStatement(sql);
+		preparedStatement.setString(1, email);
+		
+        resultSet = preparedStatement.executeQuery();
+        List<String> Question = new ArrayList<String>();
+        while (resultSet.next()) {
+            Question.add(resultSet.getString("questionid"));
+        }
+        preparedStatement.close();
+        statement.close();
+        disconnect();
+        return Question;
+        }
+}
